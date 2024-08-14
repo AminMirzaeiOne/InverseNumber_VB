85ec96fac3fb4fb87d2b970ef06fac9f46ff23d9
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-14 15:25:42 +0330

    Add project files.

---
+++ InverseNumber/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim x, r, s As Integer
+        System.Console.ForegroundColor = ConsoleColor.Cyan
+        System.Console.Write("Pls Enter Number: ")
+        System.Console.ForegroundColor = ConsoleColor.Yellow
+        x = System.Convert.ToInt32(Console.ReadLine())
+
+        s = 0
+
+        Do
+            r = x Mod 10
+
+            s = s * 10 + r
+
+            x = x \ 10
+        Loop While x > 0
+
+        System.Console.WriteLine()
+
+        System.Console.ForegroundColor = ConsoleColor.Blue
+        System.Console.Write("Reverse Number Is: ")
+        System.Console.BackgroundColor = ConsoleColor.Blue
+        System.Console.ForegroundColor = ConsoleColor.White
+        System.Console.WriteLine(s)
+
+        System.Console.ResetColor()
+
+        System.Console.ForegroundColor = ConsoleColor.Magenta
+        System.Console.WriteLine("-----------------------")
+        System.Console.ForegroundColor = ConsoleColor.Red
+        System.Console.WriteLine("Programmer : Amin Mirzaei - Github(UserName) : AminMirzaeiOne")
+
+        System.Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ InverseNumber/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("InverseNumber")>
+<Assembly: AssemblyDescription("Among the first projects of Amin Mirzaei after learning the basics of Visual Basic.NET")>
+<Assembly: AssemblyCompany("AminMirzaeiOne")>
+<Assembly: AssemblyProduct("InverseNumber")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("AminMirzaeiOne")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d82947d9-9728-4273-9ddc-a7d79f461539")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
